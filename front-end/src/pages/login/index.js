27cc47f08b003b647945a3ccb4d30697ba71b0e2
joaopedrobarros777 <66282957+joaopedrobarros777@users.noreply.github.com>
2020-11-10 21:28:19 -0300

    logo

--- front-end/src/pages/login/index.js
+++ front-end/src/pages/login/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../../components/img/logo.png'
 
 const Login = () => {
     return (
         <>
-
+            <img src={logo} alt='Logo SnitchTracker com simbolo de átomo em azul'/>
         </>
     )
 }
